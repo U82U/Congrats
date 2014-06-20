d1185f7691ec838883e55866ae3a225b84f4a9b8
U82U <sidorovichev@gmail.com>
2014-06-20 11:34:44 +0300

    Added Ninject

--- Congrats/Global.asax
+++ Congrats/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Congrats.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Congrats.WebUI.MvcApplication" Language="C#" %>
---
+++ Congrats/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace Congrats.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType != null)
+            {
+                return (IController)ninjectKernel.Get(controllerType);
+            }
+
+            return null;
+        }
+
+        protected void AddBindings()
+        {
+        }
+    }
+}
